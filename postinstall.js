931dd85cebc32f2be2493b8f3434e05e0283c71c
Jhonnatas Ribeiro <jhonnatasribeirodev@gmail.com>
2024-08-30 08:25:07 -0300

    Inserindo mais dados

---
+++ postinstall.js
@@ -0,0 +1,14 @@
+// postinstall.js
+console.log(`
+    ================================
+    🚀 O pacote 'servidor-produtos-fake-001' foi instalado com sucesso!
+    
+    Para começar a usar, siga estes passos:
+    1. Navegue para o diretório do pacote:
+       cd servidor-produtos-fake-001
+    2. Inicie o servidor com o comando:
+       npm start
+    
+    ================================
+    `);
+    
